ebae0c768ee0304d0f39c4e3027cddc91a3e8fd5
jsnzrdz <jsnzrdz@gmail.com>
2024-06-12 00:09:55 +0200

    config: update astroConfig and paths to fix the problem serving statics files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jsnzrdz.github.io',
-    base: 'cadiz-resiste',
+    site: 'https://cadizresiste.org',
+    base: '/',
 });
