9577132a93efd5f83cf18c75d0e84b580f100159
jsnzrdz <jsnzrdz@gmail.com>
2024-06-13 17:45:54 +0200

    feat: Added sanity integration getting simple posts.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+
+// tipado apra variables de entorno
+interface ImportMetaEnv {
+    readonly CONTENTFUL_SPACE_ID: string;
+    readonly CONTENTFUL_DELIVERY_TOKEN: string;
+    readonly CONTENTFUL_PREVIEW_TOKEN: string;
+}
