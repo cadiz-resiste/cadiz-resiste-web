6f8e949e7d8969cea37c36c0784a271d910aed32
jsnzrdz <jsnzrdz@gmail.com>
2024-06-13 18:39:57 +0200

    Revert "Merge branch 'contentful_integration' into development"
    This reverts commit 2629e4f6485b3475d0f66573b3a18176260d45ab, reversing
    changes made to cf5e2881ad3def406eeec9beebb7838bbbc1c5b4.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="astro/client" />
-
-// tipado apra variables de entorno
-interface ImportMetaEnv {
-    readonly CONTENTFUL_SPACE_ID: string;
-    readonly CONTENTFUL_DELIVERY_TOKEN: string;
-    readonly CONTENTFUL_PREVIEW_TOKEN: string;
-}
