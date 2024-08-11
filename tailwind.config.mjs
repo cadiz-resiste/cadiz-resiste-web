6733475a6236d1f339f552ee3255d1a22a07e1b9
Alex Núñez <alex@kintsugi-lab.com>
2024-08-11 18:26:48 +0200

    fix(package.json): fix duplicate entry for "typescript" dependency feat(common.css): add common.css file with font-face declarations and utility classes feat(CadizSkyline.astro): add CadizSkyline component with button for 'Conócenos' feat(Footer.astro): add WhatsappIcon and corresponding link to WhatsApp group feat(Header.astro): add link to 'VUTs y ATs' in header navigation style(Hero.astro): change background color class to use primary color for consistency
    style(EmailIcon.astro): Update SVG dimensions and viewBox for better consistency and alignment
    style(FacebookIcon.astro): Update SVG dimensions and viewBox for better consistency and alignment
    
    style(InstagramIcon.astro): Update SVG attributes for better readability and consistency
    style(TwitterIcon.astro): Update SVG attributes for better readability and consistency
    
    style(WhatsappIcon.astro): Update WhatsappIcon SVG attributes for better styling and compatibility
    style(HeroLayout.astro): Update import paths for common styles and assets to improve organization and maintainability
    
    feat(MainLayout, MapLayout): Update imports and file paths for Footer component and common styles to improve code organization and maintainability
    feat(MainLayout, MapLayout): Replace common.scss import with common.css for better styling consistency
    feat(MainLayout, MapLayout): Refactor layout structure to include Footer component and improve layout semantics
    feat(MainLayout, MapLayout): Update body class and layout structure for better styling and layout consistency
    feat(MainLayout, MapLayout): Remove unnecessary styles and elements from SectionsLayout and improve code cleanliness
    
    feat(piso-ilegal.astro): update import from SectionsLayout to MainLayout for better semantics
    feat(piso-ilegal.astro): adjust styles and content in piso-ilegal page for improved layout
    feat(common.scss): remove common.scss file as it is no longer needed
    feat(tailwind.config.mjs): add primary color shades and custom font families to theme in Tailwind CSS config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        "background-green": "#076633",
+        primary: {
+          50: "#EBF7F4",
+          100: "#DAF0E9",
+          200: "#A7D9C8",
+          300: "#7AC2A8",
+          400: "#349469",
+          500: "#076633",
+          600: "#065C2C",
+          700: "#044D22",
+          800: "#023D18",
+          900: "#012E10",
+          950: "#011F0A",
+        },
       },
       fontFamily: {
         kostic: ["KosticRocGrotesk", "sans-serif"],
+        archivo: ["ArchivoBlack", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
     },
